5d9781f8b84f60b0e512de3e8f80b0332680289f
semyeong@eunomia <semyeong.oh@gmail.com>
2014-02-07 20:09:13 -0500

    sample selection experiments

--- docs/notes/sample.md
+++ docs/notes/sample.md
@@ -6,7 +6,7 @@ Sample
     * 32,726 with SDSS spec
 - preselect spheroidals:
     * "bulge + disk" model fitting of 1D surface brightness profile
-    * B/(B+D) > 0.7 and ellipticity < 0.6
+    * B/(B+D) > 0.7 and ellipticity < 0.6 (equivalent to b/a > 0.4)
     * "featurelessness": ?
     * velocity dispersion > 70 km/s considering SDSS instrumental dispersion (22,621/32,726)
     * left with 2648
@@ -23,10 +23,33 @@ Nair & Abraham 2010 provides visual morph classification 0.01 < z < 0.1
 
 
 ## Experiment following Zhu+10
+*Sample 1*
 Parent sample: NSA catalog 145,155
-- red sequence: $M_g - M_i > -0.05 \times (M_r + 16.0) + 0.65$ 78,451
-- velocity dispersion > 70 km/s 40,993
+- Z < 0.05 123,396
+- velocity dispersion > 70 km/s 42,101
+- red sequence: $M_g - M_i > -0.05 \times (M_r + 16.0) + 0.65$ 
 - ellipticity: $\epsilon = 1-b/a$
     + b/a from Stokes parameters at some radius
-    + b/a from 2D Sersic fit 28,673
-        * match with RC3 to see the morphological classicifation: 207 matches <1 arcsec, 17(8%) of which are T = -5 (elliptical).
+    + b/a from 2D Sersic fit <-- used
+---> 22,378
+
+Matched with Huertas-Company+11 (sep < 1 arcsec)
+* Of 21,587 matched with HC+11, 
+
+
+
+* * *
+
+We aim to constitute a representative(complete?) sample of local massive elliptical galaxies.
+Our parent sample is the NASA-Sloan Atlas Catalog (citation) contatining galaxies
+with $z < 0.055$ within the coverage of SDSS DR8 (cite). We select this catalog for
+two main reasons:
+    1) It complements bright nearby galaxies that may be in SDSS imaging footprint, but
+    are eliminated due to difficulties in reduction (is this correct?), and adopts
+    higher threshold for distriminating stars from galaxies.
+    2) The catalog provides SDSS images with improved sky subtraction (cite), which is
+    crucial in our analysis of faint outskirts of elliptical galaxies.
+
+
+
+
---
+++ sample.py
@@ -0,0 +1,74 @@
+""" Script to select sample from NSA catalog """
+
+from pylab import *
+from astropy.table import Table
+
+rc('lines', linewidth=2)
+rc('font', size=15)
+rc('axes', grid=True)
+rc('legend', frameon=False)
+
+if 0:
+    master = Table.read('nsa_morph.fits')
+b = dict(F=0, N=1, u=2, g=3, r=4, i=5, z=6)
+
+print 'parent      : %8i' % len(master)
+# redshift
+bool_Z = master['Z_1'] < 0.05
+# Red sequence
+colorgi = master['ABSMAG'][:,b['g']] - master['ABSMAG'][:,b['i']]
+M_r = master['ABSMAG'][:,b['r']]
+bool_RS = colorgi > -0.05*(M_r + 16.0) + 0.65  # from Zhu+10
+# velocity dispersion
+bool_VD = master['VDISP'] > 70.  # SDSS instrumental broadening
+# ellipticity
+ell = 1. - master['SERSIC_BA']
+bool_Ell = ell < 0.6
+
+print 'z < 0.05    : %8i' % count_nonzero(bool_Z)
+print 'Red sequence: %8i' % count_nonzero(bool_RS & bool_Z)
+print 'VD > 70 km/s: %8i' % count_nonzero(bool_VD & bool_RS & bool_Z)
+print 'Ell < 0.6   : %8i' % count_nonzero(bool_Ell & bool_VD & bool_RS & bool_Z)
+bool_OK = bool_Ell & bool_VD & bool_RS & bool_Z
+
+# has morphology from Huertas+11
+bool_MATCH = isnan(master['id']) == False
+print 'has morph   : %8i' % count_nonzero(bool_MATCH & bool_OK)
+bool_OKM = bool_OK & bool_MATCH
+
+# prob dist of Sample 1
+ax = figure().add_subplot(111)
+bins = arange(0., 1.01, 0.1)
+for k in ['probaE', 'probaEll', 'probaS0']:
+    ax.hist(master[k][bool_OKM], histtype='step', bins=bins, label='%s' % k)
+xlabel('prob')
+ylabel('count')
+legend()
+
+bool_OKM_reject = bool_OKM & (master['probaEll'] < 0.2)
+bool_OKM_select = bool_OKM & (master['probaEll'] > 0.6)
+print 'Ell < 0.2   : %8i' % count_nonzero(bool_OKM_reject)
+print 'Ell > 0.6   : %8i' % count_nonzero(bool_OKM_select)
+
+
+# ------------
+print '-'*80
+print 'has morph   : %8i' % count_nonzero(bool_MATCH)
+
+ax = figure().add_subplot(111)
+bins = arange(0., 1.01, 0.1)
+for k in ['probaE', 'probaEll', 'probaS0']:
+    n, bins, patches = ax.hist(master[k][bool_MATCH], histtype='step', bins=bins, label='%s' % k)
+
+bool_MATCH_reject = bool_MATCH & (master['probaEll'] < 0.2)
+bool_MATCH_select = bool_MATCH & (master['probaEll'] > 0.6)
+
+print 'Ell < 0.2   : %8i' % count_nonzero(bool_MATCH_reject)
+print 'Ell > 0.6   : %8i' % count_nonzero(bool_MATCH_select)
+
+# Find commons
+print 'Ell < 0.2 common : %8i' % count_nonzero(bool_OKM_reject & bool_MATCH_reject)
+print 'Ell > 0.6 common : %8i' % count_nonzero(bool_OKM_select & bool_MATCH_select)
+
+
+
