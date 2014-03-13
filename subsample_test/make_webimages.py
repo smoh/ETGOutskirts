b133e6a3c294eaf84cb53b4045297a61cf6fc4f9
semyeong@eunomia <semyeong.oh@gmail.com>
2014-03-12 22:48:06 -0400

    add SDSS color cutout images parallel

--- subsample_test/make_webimages.py
+++ subsample_test/make_webimages.py
@@ -1,7 +1,8 @@
 import jinja2
 from glob import glob
-from astropy.table import Table
+from astropy.table import Table, Column
 import argparse
+from numpy import sign
 
 parser = argparse.ArgumentParser(
             description="make html page of fitting result")
@@ -56,7 +57,10 @@ template = jinja2.Template(
                 
             </table>
         </td>
-        <td><img width="80%" src="{{ '%s' % (plotdir) }}/{{ row['NAME'] }}.png"></td>
+        <td>
+            <img width="80%" src="{{ '%s' % (plotdir) }}/{{ row['NAME'] }}.png">
+            <img src="http://skyservice.pha.jhu.edu/DR9/ImgCutout/getjpeg.aspx?ra={{radec[loop.index0][0]}}&dec={{radec[loop.index0][1]}}">
+        </td>
     </tr>
     {% endfor %}
 </table>
@@ -69,8 +73,28 @@ result = Table.read(args.result)
 profiles = [x.split('FIT_')[-1] for x in result.colnames if 'FIT_' in x]
 if len(profiles) > 1:
     raise 'More than one profile found'
+# ra, dec from IAUNAME
+def iauname2radec(name):
+    sig = '-' if '-' in name else '+'
+    rastr = name.split(sig)[0][1:]
+    decstr = name.split(sig)[-1]
+    rahr = float(rastr[:2])
+    ramin = float(rastr[2:4])
+    rasec = float(rastr[4:])
+    radeg = rahr*15. + ramin*15./60. + rasec*15./3600.
+    
+    decde = float(decstr[:2])
+    decmin = float(decstr[2:4])
+    decsec = float(decstr[4:])
+    decdeg = decde + decmin/60. + decsec/3600.
+    decdeg = decdeg*sign(float(sig+'1'))
 
-templatevars = {'table':result, 'profile':profiles[0], 'plotdir':args.plotdir}
+    return radeg, decdeg
+radec = [iauname2radec(name) for name in result['NAME']]
+
+
+templatevars = {'table':result, 'profile':profiles[0], 'plotdir':args.plotdir,
+                'radec':radec}
 
 with open(args.outname, 'w') as f:
     f.write(template.render(templatevars))
