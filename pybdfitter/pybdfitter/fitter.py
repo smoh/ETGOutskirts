9431a0fd6af4413a8367b537efc5ec355d5becd2
semyeong@eunomia <semyeong.oh@gmail.com>
2014-09-12 17:37:33 -0400

    clean up things to one wrapper

--- pybdfitter/pybdfitter/fitter.py
+++ pybdfitter/pybdfitter/fitter.py
@@ -1,8 +1,8 @@
 import subprocess
-import os
+import os, sys
 import errno
 
-__all__ = ['fit_sample', 'fit_sample_stream']
+__all__ = ['fit_sample']
 
 
 def mkdir_p(path):
@@ -16,8 +16,7 @@ def mkdir_p(path):
 
 
 def build_command_str(input, profile, start, end, outdir, datadir='data',
-                      imgdir=None, filter='r', residual=True, debug=True,
-                      savepsf=False):
+                      imgdir=None, filter='r', residual=True, debug=True):
     """
     Build idl command string for FIT_SAMPLE
     """
@@ -30,8 +29,6 @@ def build_command_str(input, profile, start, end, outdir, datadir='data',
         cmd += ', /residuals'
     if debug:
         cmd += ', /debug'
-    if savepsf:
-        cmd += ', /savepsf'
     if imgdir:
         cmd += ", imgdir='{:s}'".format(imgdir + '/')
     return cmd
@@ -47,52 +44,30 @@ def check_IDL():
 
 
 def fit_sample(input, profile, start, end, outdir,
-               datadir='data', imgdir=None, output=None,
-               filter='r', residual=True, debug=True, savepsf=False,
-               stdout=None, stderr=None):
-    if check_IDL():
-        print "NO IDL found. Nothing else done"
-        return 0
-
-    # prepare directories
-    mkdir_p(outdir)
-    if residual:
-        mkdir_p(outdir + '/models')
-    if savepsf:
-        mkdir_p(datadir + '/sdss_psf')
-
-    cmd = build_command_str(
-        input, profile, start, end, outdir,
-        datadir=datadir, imgdir=imgdir, filter=filter, residual=residual,
-        debug=debug, savepsf=savepsf)
-    proc = subprocess.Popen(["idl", "-e", cmd],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = proc.communicate()
-    if out[0]:
-        print out[0]
-    if proc.returncode:
-        print out[1]
-        return 0
-
-    out_expected = 'RAWFIT%05i.%05i.fits' % (start, end)
-    if output:
-        os.rename(out_expected, output)
-    outTable = outdir + '/' + [out_expected if not output else output][0]
-    return outTable
-
-
-def fit_sample_stream(input, profile, start, end, outdir,
                       datadir='data', imgdir=None, output=None,
-                      filter='r', residual=True, debug=True, savepsf=False,
+                      filter='r', residual=True, debug=True, 
                       stdout=None, stderr=None, quiet=False):
     """
     Run bdfitter
 
-    input : str, input file name
-    profile : str, 'profile_name:number_of_params' e.g., 'DVC:8'
-    start : int, start row
-    end : int, end row (inclusive)
-    outdir : str, output directory
+    input : str
+        input file name
+    profile : str
+        'profile_name:number_of_params' e.g., 'DVC:8'
+    start : int
+        start row
+    end : int
+        end row (inclusive)
+    outdir : str
+        output directory
+    datadir : str, default='data'
+        data root directory
+    imgdir : str, optional
+        set in case images are not in [datadir]/images
+    output : str, optional
+        output table name. If not given, it will be name RAWFITXXX..
+    residual : bool, default=True
+        save model images?
     """
     if check_IDL():
         print "NO IDL found. Nothing else done"
@@ -101,13 +76,11 @@ def fit_sample_stream(input, profile, start, end, outdir,
     mkdir_p(outdir)
     if residual:
         mkdir_p(outdir + '/models')
-    if savepsf:
-        mkdir_p(datadir + '/sdss_psf')
 
     cmd = build_command_str(
         input, profile, start, end, outdir,
         datadir=datadir, imgdir=imgdir, filter=filter, residual=residual,
-        debug=debug, savepsf=savepsf)
+        debug=debug)
     if quiet:
         proc = subprocess.Popen(["idl", "-e", cmd, "-quiet"],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -115,11 +88,13 @@ def fit_sample_stream(input, profile, start, end, outdir,
         proc = subprocess.Popen(["idl", "-e", cmd],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         for line in iter(proc.stdout.readline, b''):
-            print ' STDOUT '.center(80, '=')
+            # print ' STDOUT '.center(80, '=')
             print line,
+            sys.stdout.flush()
+            sys.stderr.flush()
     out = proc.communicate()  # close p.stdout, wait for the subprocess to exit
-    if proc.returncode:  # if fails
-        print proc.stderr
+    # if proc.returncode:  # if fails
+    print proc.stderr
 
     out_expected = 'RAWFIT%05i.%05i.fits' % (start, end)
     if output:
