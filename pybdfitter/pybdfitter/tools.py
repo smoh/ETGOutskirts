bb09dfe360270768d57145f82f719b297ac1bd28
Semyeong Oh <semyeong.oh@gmail.com>
2014-10-02 14:40:37 -0400

    initial commit

---
+++ pybdfitter/pybdfitter/tools.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.stats import binned_statistic
+
+__all__ = ["radialprofile"]
+
+def radialprofile(image, center=None, binsize=2):
+    """Calculate radial profile from an image
+
+    image : 2d array
+        input image array
+    center : (x, y)
+        center coordinate in pixels
+        if None, use the center of the image
+    binsize : float
+        size of radius bin
+
+    Returns
+    bin_centers, values
+
+    """
+    # Calculate the indices from the image
+    y, x = np.indices(image.shape)
+
+    if center is None:
+        center = np.array([(x.max()-x.min())/2.0, (y.max()-y.min())/2.0])
+
+    r = np.hypot(x - center[0], y - center[1])
+
+    bins = np.arange(0, r.max(), binsize)
+    bin_centers = (bins[1:] + bins[:-1]) * 0.5
+
+    p = binned_statistic(r.ravel(), image.ravel(), bins=bins)[0]
+
+    return bin_centers, p
+
+
