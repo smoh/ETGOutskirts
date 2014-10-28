3fbd56f8e58974c21fe1b0cd51a0bcf4a6828f52
Semyeong Oh <semyeong.oh@gmail.com>
2014-10-28 01:20:24 -0400

    add EllipsePlot class

--- pybdfitter/pybdfitter/ellipse.py
+++ pybdfitter/pybdfitter/ellipse.py
@@ -5,8 +5,9 @@ collection of tools to interact with iraf ellipse
 import os, shutil, uuid, re
 from pyraf import iraf
 from astropy.table import Table
+from matplotlib import pyplot as plt
 
-__all__ = ['ellipse', 'read_ellipse_output']
+__all__ = ['ellipse', 'read_ellipse_output', 'EllipsePlot']
 
 def shorten_iauname(name):
     p = re.compile('(J[\d.]+)[+-][\d.]+')
@@ -83,3 +84,56 @@ def read_ellipse_output(filename):
     return Table.read(
         filename, format='ascii.commented_header', header_start=1, data_start=0,
         fill_values=['INDEF', '-99'])
+
+
+class EllipsePlot(object):
+    def __init__(self, out):
+        self.fn = out
+        self.t = read_ellipse_output(out)
+        self.badexit = self.t['STOP'] != 0
+        self.good = self.t['RMS'] < self.t['INTENS']
+
+    def setup_figure(self):
+        fig = plt.figure(figsize=(6, 8))
+        fig.subplots_adjust(left=0.18, right=0.95, wspace=.4, top=.95)
+        axIntens = plt.subplot2grid((4,1), (0,0), rowspan=2)
+        axPA     = plt.subplot2grid((4,1), (2,0), sharex=axIntens)
+        axEllip  = plt.subplot2grid((4,1), (3,0), sharex=axIntens)
+
+        self.fig = fig
+        self.plot_intens(axIntens)
+        self.plot_pa(axPA)
+        self.plot_ellip(axEllip)
+
+    def plot_intens(self, ax=None):
+        t = self.t
+        ax.errorbar(t['SMA'], t['INTENS'], yerr=t['RMS'], fmt='o-')
+        ax.plot(t['SMA'][self.badexit], t['INTENS'][self.badexit], 'ks', mfc='None', ms=12)
+        ax.set_yscale('log')
+
+    def plot_pa(self, ax=None):
+        t = self.t
+        pa = t['PA']
+        # pa[pa<0] = pa[pa<0] +180.
+        ax.errorbar(t['SMA'], pa, yerr=t['PA_ERR'], fmt='o-')
+        ax.plot(t['SMA'][self.badexit], pa[self.badexit], 'ro', mfc='None', ms=12)
+        ax.set_ylabel('PA')
+
+    def plot_ellip(self, ax=None):
+        t = self.t
+        ax.errorbar(t['SMA'], t['ELLIP'], yerr=t['ELLIP_ERR'], fmt='o-')
+        ax.plot(t['SMA'][self.badexit], t['ELLIP'][self.badexit], 'ro', mfc='None', ms=12)
+        ax.plot(t['SMA'], t['A_BIG']/t['RMS'], 'ko:')
+        ax.set_ylabel('Ellipticity')
+
+    def plot_3(self, ax=None):
+        t = self.t
+        ax.errorbar(t['SMA'], t['A3'], yerr=t['A3_ERR'], fmt='o-')
+        ax.errorbar(t['SMA'], t['B3'], yerr=t['B3_ERR'], fmt='o-')
+        ax.set_ylabel('A3, B3')
+
+    def plot_4(self, ax=None):
+        t = self.t
+        ax.errorbar(t['SMA'], t['A4'], yerr=t['A4_ERR'], fmt='o-')
+        ax.errorbar(t['SMA'], t['B4'], yerr=t['B4_ERR'], fmt='o-')
+        ax.set_ylabel('A4, B4')
